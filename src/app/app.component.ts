7bbc385612b3028d844b42033cd2c840f9a52c44
ismet kizgin <info@ismetkizgin.com>
2020-10-25 00:47:51 +0300

    feat: multiple language support added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from './utils';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
+  constructor(
+    private _translateService: TranslateService,
+    private _languageService: LanguageService
+  ) {
+    this._translateService.addLangs(['tr', 'en']);
+    this._translateService.setDefaultLang('tr');
+
+    if (this._languageService.getLanguage()) {
+      this._translateService.use(localStorage.getItem('language'));
+    } else {
+      this._translateService.use(this._translateService.getBrowserLang());
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
