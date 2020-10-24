c8ec6300517d6e8bdbbd1f06fef81ebcf9869fef
ismet kizgin <info@ismetkizgin.com>
2020-10-25 00:52:43 +0300

    feat: added dynamic title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from './utils';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +12,10 @@ import { LanguageService } from './utils';
 export class AppComponent {
   constructor(
     private _translateService: TranslateService,
-    private _languageService: LanguageService
+    private _titleService: Title,
+    private _router: Router,
+    private _languageService: LanguageService,
+    private _activatedRoute: ActivatedRoute
   ) {
     this._translateService.addLangs(['tr', 'en']);
     this._translateService.setDefaultLang('tr');
@@ -19,5 +25,32 @@ export class AppComponent {
     } else {
       this._translateService.use(this._translateService.getBrowserLang());
     }
+  }
+
+  ngOnInit(): void {
+    this._router.events
+      .pipe(
+        map(() => {
+          let child = this._activatedRoute.firstChild;
+          while (child) {
+            if (child.firstChild) {
+              child = child.firstChild;
+            } else if (child.snapshot.data && child.snapshot.data['title']) {
+              return child.snapshot.data['title'];
+            } else {
+              return null;
+            }
+          }
+        })
+      )
+      .subscribe((title) => {
+        this._translateService.get('ProjectName').subscribe((projectName) => {
+          title != null
+            ? this._translateService.get(title).subscribe((value) => {
+                this._titleService.setTitle(`${value} - ${projectName}`);
+              })
+            : this._titleService.setTitle(`${projectName}`);
+        });
+      });
   }
 }
