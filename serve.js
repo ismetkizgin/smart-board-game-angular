19a0c9488339277aa5a6a1ca0f238e307ba3156c
ismet kizgin <info@ismetkizgin.com>
2020-10-25 01:40:02 +0300

    ci: heroku deploy configuration applied

---
+++ serve.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/smart-board-game'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/smart-board-game/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
