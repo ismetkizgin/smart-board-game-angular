42339e317fbb2a76ebdee1c4a123299990f9b14b
ismet kizgin <info@ismetkizgin.com>
2020-10-26 01:51:26 +0300

    feat: box select trigger added.

--- src/app/views/game/game.component.ts
+++ src/app/views/game/game.component.ts
@@ -1,29 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) document) {}
 
-  constructor() { }
+  boardVerticalSize: Array<number>;
+  boardHorizontalSize: Array<number>;
+  stonePositions: Array<number> = [0, 12];
+  boardSize: number;
+  ngOnInit(): void {
+    this.boardVerticalSize = Array(5)
+      .fill(null)
+      .map((x, i) => i);
 
-  tahta:Array<{name:string}>=[];
-  tahtaSize:number;
+    this.boardHorizontalSize = Array(5)
+      .fill(null)
+      .map((x, i) => i);
+    this.boardSize =
+      this.boardHorizontalSize.length * this.boardVerticalSize.length;
+  }
 
-  createTable(value)
-  {
-    this.tahta=[];
-    for(let i=0;i<value;i+=1)
-    {
-      if(i%2==0)
-      this.tahta.push({name:'siyah'});
-      else
-      this.tahta.push({name:'beyaz'});
+  selectBoxID: number = null;
+  onSubmit(boxID) {
+    for (let i = 0; i < this.boardSize; i++) {
+      document.getElementById(`box${i}`).classList.remove('board__box__green');
+    }
+    console.log(this.selectBoxID);
+    const selectBox: any = document.getElementById(`box${boxID}`);
+    if (selectBox.getElementsByTagName('span').length) {
+      if (
+        boxID + 1 >= 0 &&
+        boxID + 1 < this.boardSize &&
+        boxID % this.boardHorizontalSize.length != 4
+      )
+        document
+          .getElementById(`box${boxID + 1}`)
+          .classList.add('board__box__green');
+
+      if (
+        boxID - 1 >= 0 &&
+        boxID - 1 < this.boardSize &&
+        boxID % this.boardHorizontalSize.length != 0
+      )
+        document
+          .getElementById(`box${boxID - 1}`)
+          .classList.add('board__box__green');
+
+      if (
+        boxID - this.boardVerticalSize.length > 0 &&
+        boxID - this.boardVerticalSize.length < this.boardSize
+      )
+        document
+          .getElementById(`box${boxID - this.boardVerticalSize.length}`)
+          .classList.add('board__box__green');
+
+      if (
+        boxID + this.boardVerticalSize.length > 0 &&
+        boxID + this.boardVerticalSize.length < this.boardSize
+      )
+        document
+          .getElementById(`box${boxID + this.boardVerticalSize.length}`)
+          .classList.add('board__box__green');
+
+      this.selectBoxID = boxID;
+    } else if (this.selectBoxID) {
+      console.log('else');
+    } else {
+      this.selectBoxID = null;
     }
   }
-  ngOnInit(): void {
-  }
-
 }
