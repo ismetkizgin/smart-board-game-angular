3db7fcc581da2c9715f99265e217d57b9b4be8fe
ismet kizgin <info@ismetkizgin.com>
2020-11-09 15:17:40 +0300

    refactor: game board revised

--- src/app/views/game/game.component.ts
+++ src/app/views/game/game.component.ts
@@ -15,11 +15,11 @@ export class GameComponent implements OnInit {
   stonePositions: Array<number> = [0, 12];
   boardSize: number;
   ngOnInit(): void {
-    this.boardVerticalSize = Array(9)
+    this.boardVerticalSize = Array(8)
       .fill(null)
       .map((x, i) => i);
 
-    this.boardHorizontalSize = Array(9)
+    this.boardHorizontalSize = Array(8)
       .fill(null)
       .map((x, i) => i);
     this.boardSize =
@@ -33,53 +33,62 @@ export class GameComponent implements OnInit {
     }
     const selectBox: any = document.getElementById(`box${boxID}`);
     if (selectBox.getElementsByTagName('span').length) {
-      if (
-        boxID + 1 >= 0 &&
-        boxID + 1 < this.boardSize &&
-        boxID % this.boardHorizontalSize.length != this.boardHorizontalSize.length - 1
-      )
-        document
-          .getElementById(`box${boxID + 1}`)
-          .classList.add('board__box__green');
-
-      if (
-        boxID - 1 >= 0 &&
-        boxID - 1 < this.boardSize &&
-        boxID % this.boardHorizontalSize.length != 0
-      )
-        document
-          .getElementById(`box${boxID - 1}`)
-          .classList.add('board__box__green');
-
-      if (
-        boxID - this.boardVerticalSize.length >= 0 &&
-        boxID - this.boardVerticalSize.length < this.boardSize
-      )
-        document
-          .getElementById(`box${boxID - this.boardVerticalSize.length}`)
-          .classList.add('board__box__green');
-
-      if (
-        boxID + this.boardVerticalSize.length >= 0 &&
-        boxID + this.boardVerticalSize.length < this.boardSize
-      )
-        document
-          .getElementById(`box${boxID + this.boardVerticalSize.length}`)
-          .classList.add('board__box__green');
-
+      this.markPathOfMotion(boxID);
       this.selectBoxID = boxID;
     } else if (
       this.selectBoxID != null &&
       (this.selectBoxID + this.boardVerticalSize.length == boxID ||
         this.selectBoxID - this.boardVerticalSize.length == boxID ||
-        this.selectBoxID + 1 == boxID ||
-        this.selectBoxID - 1 == boxID)
+        (this.selectBoxID + 1 == boxID &&
+          this.selectBoxID % this.boardHorizontalSize.length !=
+            this.boardHorizontalSize.length - 1) ||
+        (this.selectBoxID - 1 == boxID &&
+          this.selectBoxID % this.boardHorizontalSize.length != 0))
     ) {
-      selectBox.innerHTML = document.getElementById(`box${this.selectBoxID}`).innerHTML;
+      selectBox.innerHTML = document.getElementById(
+        `box${this.selectBoxID}`
+      ).innerHTML;
       document.getElementById(`box${this.selectBoxID}`).innerHTML = null;
       this.selectBoxID = null;
     } else {
       this.selectBoxID = null;
     }
+  }
+
+  markPathOfMotion(boxID) {
+    if (
+      boxID + 1 >= 0 &&
+      boxID + 1 < this.boardSize &&
+      boxID % this.boardHorizontalSize.length !=
+        this.boardHorizontalSize.length - 1
+    )
+      document
+        .getElementById(`box${boxID + 1}`)
+        .classList.add('board__box__green');
+
+    if (
+      boxID - 1 >= 0 &&
+      boxID - 1 < this.boardSize &&
+      boxID % this.boardHorizontalSize.length != 0
+    )
+      document
+        .getElementById(`box${boxID - 1}`)
+        .classList.add('board__box__green');
+
+    if (
+      boxID - this.boardVerticalSize.length >= 0 &&
+      boxID - this.boardVerticalSize.length < this.boardSize
+    )
+      document
+        .getElementById(`box${boxID - this.boardVerticalSize.length}`)
+        .classList.add('board__box__green');
+
+    if (
+      boxID + this.boardVerticalSize.length >= 0 &&
+      boxID + this.boardVerticalSize.length < this.boardSize
+    )
+      document
+        .getElementById(`box${boxID + this.boardVerticalSize.length}`)
+        .classList.add('board__box__green');
   }
 }
